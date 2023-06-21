e2bb16f5ffcd7df68ec4effa795e7d8ee5818f95
tarerahul1008 <tarerahul1008@gmail.com>
2023-06-21 13:49:22 +0530

    added subtraction method

--- src/com/demo/Parent.java
+++ src/com/demo/Parent.java
@@ -4,5 +4,10 @@ public class Parent {
 	void addition() {
 		System.out.println("Calculate two integer");
 	}
+	
+	void Subtraction()
+	{
+		System.out.println("Subtraction of two no. is ");
+	}
 
 }
